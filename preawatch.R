fd356567b9b0a18890e435fd11e9bf4b204d6de4
jessicakay <52629990+jessicakay@users.noreply.github.com>
2020-08-11 20:33:57 -0400

    import code

--- preawatch.R
+++ preawatch.R
@@ -1,13 +1,21 @@
 # PREA watch
 # github.com/jessicakay
 
+# setup - run once per machine
+
 install.packages("googlesheets")
 install.packages("reshape2")
+
+# startup - run on load
+
 library(reshape2)
+fileDir<-"~/Downloads/PREA Report.xlsx"
 
-original_state<-readxl::read_xlsx("~/Downloads/PREA Report.xlsx",sheet = 2)
-
+original_state<-readxl::read_xlsx(fileDir,sheet = 2)
+originalcounty<-readxl::read_xlsx(fileDir,sheet = 4)
 temp_state<-as.data.frame(original_state)
+tempcounty<-as.data.frame(originalcounty)
 
+# data cleaning 
 colnames(temp_state)[1]<-"variable"
 colnames(temp_state)
