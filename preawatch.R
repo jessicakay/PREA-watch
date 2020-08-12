de9b18c18d30d6e01402576b0765beca590bab90
jessicakay <52629990+jessicakay@users.noreply.github.com>
2020-08-11 21:20:49 -0400

    Update preawatch.R

--- preawatch.R
+++ preawatch.R
@@ -5,10 +5,12 @@
 
 install.packages("googlesheets")
 install.packages("reshape2")
+install.packages("dplyr")
 
 # startup - run on load
 
 library(reshape2)
+library(dplyr)
 fileDir<-"~/Downloads/PREA Report.xlsx"
 
 original_state<-readxl::read_xlsx(fileDir,sheet = 2)
@@ -17,5 +19,25 @@ temp_state<-as.data.frame(original_state)
 tempcounty<-as.data.frame(originalcounty)
 
 # data cleaning 
+
 colnames(temp_state)[1]<-"variable"
-colnames(temp_state)
+
+e<-tolower(colnames(temp_state))
+colnames(temp_state)<-e
+
+
+var_names <- c(
+  "Inmate on inmate sex acts",
+  "Inmate on inmate sex abuse",
+  "Inmate on inmate sexual harrassment",
+  "Staff sexual misconduct",
+  "Staff-inamte sexual harassment"
+  )
+
+state_data <- as.data.frame(
+  temp_state %>%
+    select(variable, substantiated, pending, unfounded, unsubstantiated) %>%
+    filter(variable %in% var_names)
+)
+
+View(state_data)
