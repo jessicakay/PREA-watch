3d891284aadebd24d352f31da8ce50264e58333a
jessicakay <52629990+jessicakay@users.noreply.github.com>
2020-08-16 15:35:28 -0400

    Update preawatch.R

--- preawatch.R
+++ preawatch.R
@@ -25,7 +25,6 @@ colnames(temp_state)[1]<-"variable"
 e<-tolower(colnames(temp_state))
 colnames(temp_state)<-e
 
-
 var_names <- c(
   "Inmate on inmate sex acts",
   "Inmate on inmate sex abuse",
@@ -40,36 +39,33 @@ state_data <- as.data.frame(
     filter(variable %in% var_names | str_detect(variable, "[[:digit:]]+") == TRUE)
 )
 
+# a<-as.data.frame(str_extract_all(state_data[yearRows,][1],"[[:digit:]]+"))
 
-a<-as.data.frame(str_extract_all(state_data[yearRows,][1],"[[:digit:]]+"))
+state_data[yearRows,]$variable # list of header rows
 
-state_data[yearRows,]$variable
+state_data <-state_data %>% 
+  mutate(year = case_when(
+    str_detect(state_data$variable, "[[:digit:]]+") == TRUE ~  
+      str_extract(state_data$variable, "[[:digit:]]+")
+  )
+  )
 
-# temp_state$variable[which(
-#  str_detect(temp_state$variable,"[[:digit:]]+\\s-[[:alpha:]]+")
-#  )]<-temp_state$variable[which(str_detect(temp_state$variable,"[[:digit:]]+\\s-[[:alpha:]]+"))]
-i<0
-for(i in dim(state_data)[1]){
-if(str_detect(state_data$variable[i],"[[:digit:]]+\\s-[[:alpha:]]+")==TRUE){
-  state_data$variable[i]<-"a"
-}
-  i<-i+1
-}
 
-View(
-  state_data %>% 
-    mutate(year = case_when(
-      str_detect(state_data$variable, "[[:digit:]]+") == TRUE ~  
-        str_extract(state_data$variable, "[[:digit:]]+")
-       )
-    )
-)
+# i<0
+# for(i in dim(state_data)[1]){
+#  if(str_detect(state_data$year[i],"[[:digit:]]+")==TRUE){
+#    state_data$variable[i+1]<-"a"
+#  }
+#  i<-i+1
+# }
 
-str_detect(state_data$variable,"[[:digit:]]+\\s-[[:alpha:]]+")==TRUE
 
 
 yearRows<-which(str_detect(state_data$variable,"[[:digit:]]+\\s-[[:alpha:]]+")==TRUE)
 
-state_data[yearRows,]<-state_data$variable[yearRows,]
 
-state_data$ str_extract(state_data[yearRows,]$variable,"[[:digit:]]+")
+
+state_data[yearRows+1,]$year<-state_data[yearRows,]$year
+state_data[yearRows+2,]$year<-state_data[yearRows,]$year
+state_data[yearRows+3,]$year<-state_data[yearRows,]$year
+state_data[yearRows+4,]$year<-state_data[yearRows,]$year
