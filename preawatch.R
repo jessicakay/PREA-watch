8bde843497481697a42df1e78b45b641d656b293
jessicakay <52629990+jessicakay@users.noreply.github.com>
2020-08-16 15:19:18 -0400

    Update preawatch.R

--- preawatch.R
+++ preawatch.R
@@ -40,21 +40,36 @@ state_data <- as.data.frame(
     filter(variable %in% var_names | str_detect(variable, "[[:digit:]]+") == TRUE)
 )
 
-View(state_data)
 
-library(stringr)
+a<-as.data.frame(str_extract_all(state_data[yearRows,][1],"[[:digit:]]+"))
 
-state_data[yearRows,][1]
+state_data[yearRows,]$variable
 
-as.data.frame(str_extract_all(state_data[yearRows,][1],"[[:digit:]]+"))
+# temp_state$variable[which(
+#  str_detect(temp_state$variable,"[[:digit:]]+\\s-[[:alpha:]]+")
+#  )]<-temp_state$variable[which(str_detect(temp_state$variable,"[[:digit:]]+\\s-[[:alpha:]]+"))]
+i<0
+for(i in dim(state_data)[1]){
+if(str_detect(state_data$variable[i],"[[:digit:]]+\\s-[[:alpha:]]+")==TRUE){
+  state_data$variable[i]<-"a"
+}
+  i<-i+1
+}
 
-temp_state$variable[which(
-  str_detect(temp_state$variable,"[[:digit:]]+\\s-[[:alpha:]]+")
-  )]<-temp_state$variable[which(str_detect(temp_state$variable,"[[:digit:]]+\\s-[[:alpha:]]+"))]
+View(
+  state_data %>% 
+    mutate(year = case_when(
+      str_detect(state_data$variable, "[[:digit:]]+") == TRUE ~  
+        str_extract(state_data$variable, "[[:digit:]]+")
+       )
+    )
+)
+
+str_detect(state_data$variable,"[[:digit:]]+\\s-[[:alpha:]]+")==TRUE
 
 
 yearRows<-which(str_detect(state_data$variable,"[[:digit:]]+\\s-[[:alpha:]]+")==TRUE)
 
-temp_state[yearRows,]
+state_data[yearRows,]<-state_data$variable[yearRows,]
 
-str_extract(state_data$variable,"[[:digit:]]+")
+state_data$ str_extract(state_data[yearRows,]$variable,"[[:digit:]]+")
