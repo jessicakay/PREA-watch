8b3c1a90b11e51c246f94343ab069a48dc81a935
jessicakay <52629990+jessicakay@users.noreply.github.com>
2020-09-06 16:55:19 -0400

    Update preawatch.R

--- preawatch.R
+++ preawatch.R
@@ -13,6 +13,8 @@ library(reshape2)
 library(dplyr)
 library(stringr)
 
+attribution <- "github.com/jessicakay/crimeviz"
+
 fileDir<-"~/Downloads/PREA Report.xlsx"
 
 original_state<-readxl::read_xlsx(fileDir,sheet = 2)
@@ -37,7 +39,8 @@ var_names <- c(
   "Inmate on inmate sex abuse",
   "Inmate on inmate sexual harassment",
   "Staff sexual misconduct",
-  "Staff-inmate sexual harassment"
+  "Staff-inmate sexual harassment",
+  "Average Daily Population"
   )
 
 state_data <- as.data.frame(
@@ -62,38 +65,42 @@ yearRows<-which(str_detect(state_data$variable,"[[:digit:]]+")==TRUE)
 # extract facility names
 
 yearFacs<-str_extract(state_data[yearRows,]$variable,"[[:alpha:]]+\\(?\\s[[:alpha:]]+")
-
 state_data$facility<-"facility"
+state_data$adp<-"adp"
 state_data[yearRows+1,]$year<-state_data[yearRows,]$year 
 state_data[yearRows+2,]$year<-state_data[yearRows,]$year 
 state_data[yearRows+3,]$year<-state_data[yearRows,]$year
 state_data[yearRows+4,]$year<-state_data[yearRows,]$year
 state_data[yearRows+5,]$year<-state_data[yearRows,]$year
+state_data[yearRows+6,]$year<-state_data[yearRows,]$year
+state_data[yearRows,]$adp<-state_data[yearRows+6,]$substantiated
+state_data[yearRows+1,]$adp<-state_data[yearRows+6,]$substantiated
+state_data[yearRows+2,]$adp<-state_data[yearRows+6,]$substantiated
+state_data[yearRows+3,]$adp<-state_data[yearRows+6,]$substantiated
+state_data[yearRows+4,]$adp<-state_data[yearRows+6,]$substantiated
+state_data[yearRows+5,]$adp<-state_data[yearRows+6,]$substantiated
 state_data[yearRows+1,]$facility<-str_extract(state_data[yearRows,]$variable,"[[:alpha:]]+\\(?\\s[[:alpha:]]+")
 state_data[yearRows+2,]$facility<-str_extract(state_data[yearRows,]$variable,"[[:alpha:]]+\\(?\\s[[:alpha:]]+")
 state_data[yearRows+3,]$facility<-str_extract(state_data[yearRows,]$variable,"[[:alpha:]]+\\(?\\s[[:alpha:]]+")
 state_data[yearRows+4,]$facility<-str_extract(state_data[yearRows,]$variable,"[[:alpha:]]+\\(?\\s[[:alpha:]]+")
 state_data[yearRows+5,]$facility<-str_extract(state_data[yearRows,]$variable,"[[:alpha:]]+\\(?\\s[[:alpha:]]+")
-
-
-
-# future loop to cut down on redundant code
-# 
-# i<0
-# for(i in dim(state_data)[1]){
-#  if(str_detect(state_data$year[i],"[[:digit:]]+")==TRUE){
-#    state_data$variable[i+1]<-"a"
-#  }
-#  i<-i+1
-# }
+state_data[yearRows+6,]$facility<-str_extract(state_data[yearRows,]$variable,"[[:alpha:]]+\\(?\\s[[:alpha:]]+")
+state_data$substantiated[str_detect(state_data$substantiated,"201[[:digit:]]")]<-NA
+new_df<-state_data[-c(yearRows,yearRows+6),] # remove header rows
 
 # convert string to numeric data
 
-new_df<-state_data[-yearRows,] # remove header rows
+new_df$substantiated<-as.numeric(new_df$substantiated)
+new_df$pending<-as.numeric(new_df$pending)
+new_df$unfounded<-as.numeric(new_df$unfounded)
+new_df$unsubstantiated<-as.numeric(new_df$unsubstantiated)
+new_df$adp<-as.numeric(new_df$adp)
+
 
 new_df[c(2,3,4,5)]<-str_extract_all(new_df[c(2,3,4,5)],"[[:digit:]]+")
 
-
+new_df$total<-as.numeric(new_df$substantiated)+as.numeric(new_df$pending)+
+  as.numeric(new_df$unfounded)+as.numeric(new_df$unsubstantiated)
 
 # render data in long format
 
@@ -103,3 +110,27 @@ View(
   melt(id.vars=c("year","facility"),
        measure.vars=c("substantiated","pending","unfounded","unsubstantiated"))
 )
+
+library(ggplot2)
+
+gridExtra::grid.arrange(
+new_df %>%
+  select(total,year) %>%
+  ggplot()+
+  stat_boxplot(geom='errorbar',aes(x=year,y=total),width=0.3)+
+  geom_boxplot(aes(x=year,y=total))+
+  labs(title = "Reporting outliers",subtitle = "Distribution of total reports per facility, stratified by year")+
+  geom_hline(yintercept = as.numeric(max(as.numeric(new_df$total),na.rm = TRUE)),color="red",linetype="dashed")+
+  annotate(geom="text",x=3,y=28,label="Souza Baranowski",color="red")+
+  geom_point(x=3,y=29,shape=1,size=10,color="red"),
+
+new_df %>% filter(variable=="Staff sexual misconduct") %>% select(total) %>%
+ggplot(aes(x=total))+
+  geom_histogram()+
+  geom_point(x=29,y=2,shape=8,size=3,color="red")+
+labs(subtitle = "Distribution of reports year year",caption = attribution)+
+  xlab("number of reports")+
+  ylab("frequency")
+)
+
+new_df %>% select(total)
